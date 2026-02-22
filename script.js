023e6a446bf76705b0952d922fa24fec4b122c9c
Md Shajjadul Ferdous <shajjadulferdous@gmail.com>
2026-02-22 21:36:26 +0600

    third push

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let interviewContainers = []
 let rejectedContainers = []
+let lastID = 'all-jobs';
 const jobContainers = document.querySelector('#jobContainers');
 
 function setValue(){
@@ -27,11 +28,117 @@ function ChangeButton(id){
      const btn = document.getElementById(id);
      btn.classList.remove('bg-white' , 'text-black');
      btn.classList.add('bg-green-500' , 'text-white');
+     const newJobContainers = document.getElementById('new-job-containers');
+     newJobContainers.innerHTML = '';
+     if (id === 'all-jobs'){
+         newJobContainers.classList.add('hidden');
+         jobContainers.classList.remove('hidden');
+     }else if (id === 'interview-jobs'){
+          jobContainers.classList.add('hidden');
+          newJobContainers.classList.remove('hidden');
+          renderInterview();
+     }else{
+           jobContainers.classList.add('hidden');
+           newJobContainers.classList.remove('hidden');
+           renderRejected();
+     }
+     lastID = id;
 }
 
-jobContainers.addEventListener('click' , function(event){
+document.querySelector('main').addEventListener('click' , function(event){
+     console.log(event.target);
      if(event.target.classList.contains('interview-btn')){
           const container = event.target.parentNode.parentNode;
-          const title = container.querySelector('')
+          const title = container.querySelector('.title').innerText;
+          const position = container.querySelector('.position').innerText;
+          const salary = container.querySelector('.salary').innerText;
+          const status = container.querySelector('.status-ase');
+          const details = container.querySelector('.details').innerText;
+          status.innerText = 'INTERVIEW';
+          const obj = {
+              title,
+              position,
+              statusValue : 'INTERVIEW',
+              salary ,
+              details
+          };
+          if(!interviewContainers.some(obj => obj.title === title)){
+              interviewContainers.push(obj);
+          }
+          console.log(interviewContainers);
+          rejectedContainers = rejectedContainers.filter(obj => obj.title !== title)
+          setValue(); 
+          ChangeButton(lastID);   
      }
-})
+     else if(event.target.classList.contains('rejected-btn')){
+          const container = event.target.parentNode.parentNode;
+          const title = container.querySelector('.title').innerText;
+          const position = container.querySelector('.position').innerText;
+          const salary = container.querySelector('.salary').innerText;
+          const status = container.querySelector('.status-ase');
+          const details = container.querySelector('.details').innerText;
+          status.innerText = 'REJECTED';
+          const obj = {
+              title,
+              position,
+              statusValue : 'REJECTED',
+              salary,
+              details
+          };
+          if(!rejectedContainers.some(obj => obj.title === title)){
+              rejectedContainers.push(obj);
+          }
+          interviewContainers = interviewContainers.filter(obj => obj.title !== title)
+          console.log(rejectedContainers);
+          ChangeButton(lastID);
+          setValue();
+     }
+});
+
+function renderInterview(){
+     const newJobContainers = document.getElementById('new-job-containers');
+     for (let newSection of interviewContainers){
+         let div = document.createElement('div');
+         div.className = `p-8 bg-white space-y-3 transition-all duration-300 hover:-translate-y-2  hover:scale-[1.02]`;
+         div.innerHTML = ` <div class="flex justify-between">
+                <div>
+                 <h1 class="title font-bold text-xl">${newSection.title}</h1>
+                 <p class="position">${newSection.position}</p>
+                </div>
+                <button class="p-4 rounded-full bg-base-200 cursor-pointer hover:bg-slate-600 hover:text-white active:scale-95"><i class="fa-solid fa-trash"></i></button>
+             </div>
+             
+             <p class="salary">${newSection.salary}</p>
+             <button class="status-ase bg-gray-300 px-4 py-2 rounded">${newSection.statusValue}</button>
+             <p class="details">${newSection.details}</p>
+             <div class="space-x-2">
+                <button  class="interview-btn px-4 py-2 font-bold border-green-400 border-2 text-green-500 transition-all duration-300 active:scale-95 hover:bg-green-500 hover:text-white hover:cursor-pointer rounded">INTERVIEW</button>
+                <button class="rejected-btn px-4 py-2 font-bold border-red-400 border-2 text-red-500 transition-all duration-300 active:scale-95 hover:bg-red-500 hover:text-white hover:cursor-pointer rounded">REJECTED</button>
+             </div>`;
+             newJobContainers.appendChild(div);
+     }
+}
+function renderRejected(){
+     const newJobContainers = document.getElementById('new-job-containers');
+     for (let newSection of rejectedContainers){
+         let div = document.createElement('div');
+         div.className = `p-8 bg-white space-y-3 transition-all duration-300 hover:-translate-y-2  hover:scale-[1.02]`;
+         div.innerHTML = ` <div class="flex justify-between">
+                <div>
+                 <h1 class="title font-bold text-xl">${newSection.title}</h1>
+                 <p class="position">${newSection.position}</p>
+                </div>
+                <button class="p-4 rounded-full bg-base-200 cursor-pointer hover:bg-slate-600 hover:text-white active:scale-95"><i class="fa-solid fa-trash"></i></button>
+             </div>
+             
+             <p class="salary">${newSection.salary}</p>
+             <button class="status-ase bg-gray-300 px-4 py-2 rounded">${newSection.statusValue}</button>
+             <p class="details">${newSection.details}</p>
+             <div class="space-x-2">
+                <button  class="interview-btn px-4 py-2 font-bold border-green-400 border-2 text-green-500 transition-all duration-300 active:scale-95 hover:bg-green-500 hover:text-white hover:cursor-pointer rounded">INTERVIEW</button>
+                <button class="rejected-btn px-4 py-2 font-bold border-red-400 border-2 text-red-500 transition-all duration-300 active:scale-95 hover:bg-red-500 hover:text-white hover:cursor-pointer rounded">REJECTED</button>
+             </div>`;
+             newJobContainers.appendChild(div);
+     }
+}
+
